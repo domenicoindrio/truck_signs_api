a8899be5f4aa0acae30a299dbcf548c096a1e70b
domenicoindrio <red.dot_curse@proton.me>
2025-08-26 16:05:14 +0200

    update for production setup

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,20 +3,19 @@ set -e
 
 echo "Waiting for postgres to connect ..."
 
-while ! nc -z db 5432; do
+while ! nc -z truck_signs_postgres 5432; do
   sleep 0.1
 done
 
 echo "PostgreSQL is active"
 
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py makemigrations
-
-gunicorn truck_signs_designs.wsgi:application --bind 0.0.0.0:8000
-
-
+python manage.py migrate
 
 echo "Postgresql migrations finished"
 
-python manage.py runserver
+gunicorn truck_signs_designs.wsgi:application --bind 0.0.0.0:8000
+
+# For local development 
+# python manage.py runserver
